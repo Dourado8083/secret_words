6e9df3d7cc79884d1154aaf2cf6ffdde773ef806
Gustavo Dourado <gustavo.dourado.br@gmail.com>
2022-10-04 22:25:54 -0300

    Finalizando tela inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./StartScreen.css"
+export const StartScreen = () => {
+  return (
+    <div className='start'><h1>Secret Word</h1>
+    <p>Clique no botão abaixo para começar o jogar</p>
+    <button>Começa o jogo</button>
+    </div>
+  )
+}
