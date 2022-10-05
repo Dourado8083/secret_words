6680ca640edf0a3e97cc3244883887b34ee49f01
Dourado8083 <72830794+Dourado8083@users.noreply.github.com>
2022-10-04 22:29:04 -0300

    Update StartScreen.js

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ import "./StartScreen.css"
 export const StartScreen = () => {
   return (
     <div className='start'><h1>Secret Word</h1>
-    <p>Clique no botão abaixo para começar o jogar</p>
+    <p>Clique no botão abaixo para começar a jogar</p>
     <button>Começa o jogo</button>
     </div>
   )
